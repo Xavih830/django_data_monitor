4d157fb854cb6e8f9ebb79e33dad87748a5a8474
Xavih830 <xhcg2003@gmail.com>
2026-01-18 22:22:41 -0500

    Cambios en whitenoise con respecto al hashing en los archivos css para que sean usados directamente por su nombre

--- backend_analytics_server/settings.py
+++ backend_analytics_server/settings.py
@@ -128,7 +128,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
+WHITENOISE_USE_FINDERS = True
+WHITENOISE_MANIFEST_STRICT = False
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
